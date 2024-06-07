d3ce5bdc41fd6b40676a5a04c3579e93d7ac0fea
Gaurav Dhyani <gauravdhyani90@gmail.com>
2024-06-08 00:43:24 +0530

    footer changed

---
+++ components/Footer.tsx
@@ -0,0 +1,9 @@
+export function Footer() {
+    return (
+        <div className="bg-gradient-to-r pt-0.5 from-blue-500 to-white absolute bottom-0 text-sm w-full text-center">
+            <h1 className="bg-black py-2">
+                Copyright &copy; 2024, ScrapQuest. All Rights Reserved.
+            </h1>
+        </div>
+    );
+}
